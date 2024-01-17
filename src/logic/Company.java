24e272424025865c5e9b556050e4be1e739a9c1b
Marcin <marcinjagielak90@gmail.com>
2024-01-17 22:18:09 +0100

    Added company class

---
+++ src/logic/Company.java
@@ -0,0 +1,15 @@
+package logic;
+
+public class Company {
+    private String name;
+    private String nip; //TODO Make it qnique
+
+    public Company(String name, String nip) {
+        this.name = name;
+        this.nip = nip;
+    }
+
+    public void showAllRentedVehicles(){
+        //TODO
+    }
+}
